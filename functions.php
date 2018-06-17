e783615605b7697aa64ab087d6ecfe1302680937
Andrew Hase <oldm8reggie@gmail.com>
2018-06-18 09:49:49 +1000

    Menu cleaned and can be extended easily

--- functions.php
+++ functions.php
@@ -12,16 +12,20 @@ function avada_lang_setup() {
 add_action( 'after_setup_theme', 'avada_lang_setup' );
 
 
-add_action( 'admin_init', 'debug_admin_menu' );
+/* add_action( 'admin_init', 'debug_admin_menu' ); */
 
-function debug_admin_menu() {
+/* function debug_admin_menu() {
 
 	echo '<pre style="padding:200px;">' . print_r( $GLOBALS[ 'menu' ], TRUE) . '</pre>';
 	//Style only to save deleting a node every time you want to look at output
 }
-
+ */
 function remove_elastic(){
 	remove_menu_page('Elastic Slider');
 	remove_menu_page('edit.php?post_type=themefusion_elastic');
+	//Located in array [100] of admin_menu, no luck removing that way 
+	remove_menu_page('Fusion Slider');
+	remove_menu_page('edit.php?post_type=slide');
+
 }
 add_action( 'admin_menu', 'remove_elastic');
